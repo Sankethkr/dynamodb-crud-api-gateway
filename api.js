2a9c9b7e71db59a1dbe90f14dd52547c173cf502
Sankethkr24 <sanketh.kr@hyniva.com>
2023-09-26 22:20:41 +0530

    Fourth commiting

--- api.js
+++ api.js
@@ -48,40 +48,24 @@ const validatePostData = (postData) => {
   }
 };
 
-const getPost = async (event) => {
+const getEmployeeDetail = async (event) => {
   const response = { statusCode: 200 };
   try {
-    const email = event.pathParameters.email; // Get the email from the URL path
-
-    if (!email || !emailRegex.test(email)) {
-      throw new Error('Invalid or missing email parameter.');
-    }
-
     const params = {
       TableName: process.env.DYNAMODB_TABLE_NAME,
-      Key: {
-        'personal email': { S: email }, // Construct the key based on your DynamoDB item structure
-      },
+      Key: marshall({ employeeId: event.pathParameters.employeeId }),
     };
-
-    const getResult = await client.send(new GetItemCommand(params));
-
-    if (!getResult.Item) {
-      throw new Error('No data found for the provided email.');
-    }
-
-    // Convert the DynamoDB item back to a JavaScript object
-    const postData = unmarshall(getResult.Item);
-
+    const { Item } = await client.send(new GetItemCommand(params));
     response.body = JSON.stringify({
-      message: 'Successfully retrieved post.',
-      postData,
+      // message: 'Successfully retrieved UserDetail.',
+      data: Item ? unmarshall(Item) : {},
+      // rawData: Item,
     });
   } catch (e) {
     console.error(e);
     response.statusCode = 500;
     response.body = JSON.stringify({
-      message: 'Failed to get post.',
+      message: 'Failed to get UserDetail.',
       errorMsg: e.message,
       errorStack: e.stack,
     });
@@ -89,103 +73,143 @@ const getPost = async (event) => {
   return response;
 };
 
+// const getPost = async (event) => {
+//   const response = { statusCode: 200 };
+//   try {
+//     const email = event.pathParameters.email; // Get the email from the URL path
 
-const createPost = async (event) => {
-  const response = { statusCode: 200 };
-  try {
-    const body = JSON.parse(event.body);
+//     if (!email || !emailRegex.test(email)) {
+//       throw new Error('Invalid or missing email parameter.');
+//     }
 
-    // Validate the incoming data
-    validatePostData(body);
+//     const params = {
+//       TableName: process.env.DYNAMODB_TABLE_NAME,
+//       Key: {
+//         'personal email': { S: email }, // Construct the key based on your DynamoDB item structure
+//       },
+//     };
 
-    const params = {
-      TableName: process.env.DYNAMODB_TABLE_NAME,
-      Item: marshall(body || {}),
-    };
-    const createResult = await client.send(new PutItemCommand(params));
-    response.body = JSON.stringify({
-      message: 'Successfully created post.',
-      createResult,
-    });
-  } catch (e) {
-    console.error(e);
-    response.statusCode = 500;
-    response.body = JSON.stringify({
-      message: 'Failed to create post.',
-      errorMsg: e.message,
-      errorStack: e.stack,
-    });
-  }
-  return response;
-};
+//     const getResult = await client.send(new GetItemCommand(params));
 
-const updatePost = async (event) => {
-  const response = { statusCode: 200 };
-  try {
-    const body = JSON.parse(event.body);
+//     if (!getResult.Item) {
+//       throw new Error('No data found for the provided email.');
+//     }
 
-    // Check if required fields are present for creating a post
-    if (!body.postId) {
-      throw new Error('Required fields are missing for updating a post.');
-    }
+//     // Convert the DynamoDB item back to a JavaScript object
+//     const postData = unmarshall(getResult.Item);
 
-    const objKeys = Object.keys(body);
-    if (objKeys.length === 0) {
-      throw new Error('No fields provided for updating the post.');
-    }
+//     response.body = JSON.stringify({
+//       message: 'Successfully retrieved post.',
+//       postData,
+//     });
+//   } catch (e) {
+//     console.error(e);
+//     response.statusCode = 500;
+//     response.body = JSON.stringify({
+//       message: 'Failed to get post.',
+//       errorMsg: e.message,
+//       errorStack: e.stack,
+//     });
+//   }
+//   return response;
+// };
 
-    // Validate the incoming data
-    validatePostData(body);
+// const createPost = async (event) => {
+//   const response = { statusCode: 200 };
+//   try {
+//     const body = JSON.parse(event.body);
 
-    const params = {
-      TableName: process.env.DYNAMODB_TABLE_NAME,
-      Key: marshall({ postId: event.pathParameters.postId }),
-      UpdateExpression: `SET ${objKeys
-        .filter((key) => key !== 'postId') // Exclude postId from updates
-        .map((_, index) => `#key${index} = :value${index}`)
-        .join(', ')}`,
-      ExpressionAttributeNames: objKeys
-        .filter((key) => key !== 'postId') // Exclude postId from updates
-        .reduce(
-          (acc, key, index) => ({
-            ...acc,
-            [`#key${index}`]: key,
-          }),
-          {}
-        ),
-      ExpressionAttributeValues: marshall(
-        objKeys
-          .filter((key) => key !== 'postId') // Exclude postId from updates
-          .reduce(
-            (acc, key, index) => ({
-              ...acc,
-              [`:value${index}`]: body[key],
-            }),
-            {}
-          )
-      ),
-    };
-    const updateResult = await client.send(new UpdateItemCommand(params));
-    response.body = JSON.stringify({
-      message: 'Successfully updated post.',
-      updateResult,
-    });
-  } catch (e) {
-    console.error(e);
-    response.statusCode = 500;
-    response.body = JSON.stringify({
-      message: 'Failed to update post.',
-      errorMsg: e.message,
-      errorStack: e.stack,
-    });
-  }
-  return response;
-};
+//     // Validate the incoming data
+//     validatePostData(body);
+
+//     const params = {
+//       TableName: process.env.DYNAMODB_TABLE_NAME,
+//       Item: marshall(body || {}),
+//     };
+//     const createResult = await client.send(new PutItemCommand(params));
+//     response.body = JSON.stringify({
+//       message: 'Successfully created post.',
+//       createResult,
+//     });
+//   } catch (e) {
+//     console.error(e);
+//     response.statusCode = 500;
+//     response.body = JSON.stringify({
+//       message: 'Failed to create post.',
+//       errorMsg: e.message,
+//       errorStack: e.stack,
+//     });
+//   }
+//   return response;
+// };
+
+// const updatePost = async (event) => {
+//   const response = { statusCode: 200 };
+//   try {
+//     const body = JSON.parse(event.body);
+
+//     // Check if required fields are present for creating a post
+//     if (!body.postId) {
+//       throw new Error('Required fields are missing for updating a post.');
+//     }
+
+//     const objKeys = Object.keys(body);
+//     if (objKeys.length === 0) {
+//       throw new Error('No fields provided for updating the post.');
+//     }
+
+//     // Validate the incoming data
+//     validatePostData(body);
+
+//     const params = {
+//       TableName: process.env.DYNAMODB_TABLE_NAME,
+//       Key: marshall({ postId: event.pathParameters.postId }),
+//       UpdateExpression: `SET ${objKeys
+//         .filter((key) => key !== 'postId') // Exclude postId from updates
+//         .map((_, index) => `#key${index} = :value${index}`)
+//         .join(', ')}`,
+//       ExpressionAttributeNames: objKeys
+//         .filter((key) => key !== 'postId') // Exclude postId from updates
+//         .reduce(
+//           (acc, key, index) => ({
+//             ...acc,
+//             [`#key${index}`]: key,
+//           }),
+//           {}
+//         ),
+//       ExpressionAttributeValues: marshall(
+//         objKeys
+//           .filter((key) => key !== 'postId') // Exclude postId from updates
+//           .reduce(
+//             (acc, key, index) => ({
+//               ...acc,
+//               [`:value${index}`]: body[key],
+//             }),
+//             {}
+//           )
+//       ),
+//     };
+//     const updateResult = await client.send(new UpdateItemCommand(params));
+//     response.body = JSON.stringify({
+//       message: 'Successfully updated post.',
+//       updateResult,
+//     });
+//   } catch (e) {
+//     console.error(e);
+//     response.statusCode = 500;
+//     response.body = JSON.stringify({
+//       message: 'Failed to update post.',
+//       errorMsg: e.message,
+//       errorStack: e.stack,
+//     });
+//   }
+//   return response;
+// };
 
 // ... rest of your code ...
 
 module.exports = {
-  getPost,
-  createPost,
-  updatePost,
+  getEmployeeDetail,
+  // createPost,
+  // updatePost,
 };
